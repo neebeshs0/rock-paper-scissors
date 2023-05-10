a71ca02464b675614204c1fe9b2bd79c636ec58a
neebeshs0 <neebeshs0@github.com>
2023-05-11 02:37:44 +0545

    Finished Project

--- index.js
+++ index.js
@@ -1,57 +1,55 @@
-let userInput = prompt("Enter rock, paper or scissors:");
-let playerSelection = userInput.toLocaleLowerCase();
+const moves = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const randomInt = Math.floor(Math.random() * (4 - 1) ) + 1;
-    console.log(randomInt);
-    switch (randomInt) {
-        case 1:
-            computerSelection = "rock";
-            break;
-        case 2:
-            computerSelection = "paper";
-            break;
-        case 3:
-            computerSelection = "scissors";
-            break;
+    randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = moves[randomNumber];
+    console.log(computerChoice);
+    return computerChoice; 
+}
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice == moves[0] && computerChoice == moves[1]) {
+        return "computerWins";
+    }
+    if (userChoice == moves[0] && computerChoice == moves[2]) {
+        return "userWins";
+    }
+    if (userChoice == moves[1] && computerChoice == moves[0]) {
+        return "userWins";
+    }
+    if (userChoice == moves[1] && computerChoice == moves[2]) {
+        return "computerWins";
+    }
+    if (userChoice == moves[2] && computerChoice == moves[0]) {
+        return "computerWins";
+    }
+    if (userChoice == moves[2] && computerChoice == moves[1]) {
+        return "userWins";
+    }
+    if (userChoice == computerChoice) {
+        return "draw";
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    computerWin = "Computer Wins";
-    playerWin = "You Win";
-    draw = "It's a draw"
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
-        return computerWin;
-    }
-    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
-        return playerWin;
-    }
-    if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "scissors") {
-        return draw;
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let userInput = prompt("Enter rock, paper or scissors:");
+        let userChoice = userInput.toLocaleLowerCase();
+        let computerChoice = getComputerChoice();
+        let score = playRound(userChoice, computerChoice);
+        if (score == "userWins") {
+            userScore++;
+        }
+        if (score == "computerWins") {
+            computerScore++;
+        }
+        if (score == "draw") {
+            console.log("its a draw");
+        }
+        console.log("You: " + userScore + " | " + "Computer: " + computerScore);
     } 
 }
 
-function game(playerSelection, computerSelection) {
-    computerScore = 0;
-    playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        x = playRound(playerSelection, computerSelection);
-        if (x == "Computer Wins") {
-            console.log("Computer wins the round!");
-            computerScore++;
-        }
-        if (x == "You Win") {
-            console.log("You win the round!");
-            playerScore++;
-        }
-        if (x == "It's a draw") {
-            console.log("It's a draw.");
-            playerScore++;
-            computerScore++;
-        }
-    }
-}
-
-game(playerSelection, getComputerChoice());
+game();
